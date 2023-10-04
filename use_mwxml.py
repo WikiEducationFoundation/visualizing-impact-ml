fe98fe5e828e7a726ec530aa02ff9f924cd59613
Megan <megan@wikiedu.org>
2023-10-04 17:49:42 +0000

    use_mwxml.py changes the XML parser to mwxml per Nate suggestion

---
+++ use_mwxml.py
@@ -0,0 +1,17 @@
+import mwxml
+import argparse
+from populate_database import insert_into_database
+
+parser = argparse.ArgumentParser(description='parse XML files with mwxml python package to format the XML data into python objects')
+parser.add_argument('-f', '--filenames', type=str, help='name of XML data file', nargs=1, required=True)
+
+dump = mwxml.Dump.from_file(open(''))
+i = 0
+for page in dump.pages:
+    revision_of_interest = next(page)
+    title = revision_of_interest.title
+    text = revision_of_interest.text
+    wikiid = revision_of_interest.id
+    id = i
+    i += 1
+    insert_into_database(id, title, text)
