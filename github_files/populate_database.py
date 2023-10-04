d2d751fd13a7e78033445d03d57b8fa17b712212
wikimegan <143218631+wikimegan@users.noreply.github.com>
2023-10-04 11:04:56 -0700

    Update populate_database.py
    reverting back to yesterday's commit

--- github_files/populate_database.py
+++ github_files/populate_database.py
@@ -1,8 +1,7 @@
 import psycopg2
 import os
-import json
 
-def insert_into_database(id, article_title, article_content):
+def insert_into_database(article_title, article_content):
     try:
         connection = psycopg2.connect(
             database="wikivi"
@@ -10,31 +9,26 @@ def insert_into_database(id, article_title, article_content):
         cursor = connection.cursor()
 
         insert_query = """
-        INSERT INTO wikipedia_articles (id, title, content)
+        INSERT INTO wikipedia_articles (title, content)
         VALUES (%s, %s);
         """
-        cursor.execute(insert_query, (id, article_title, article_content))
-
+        cursor.execute(insert_query, (article_title, article_content))
+        
         connection.commit()
-
+        
     except Exception as error:
         print(f"Error: {error}")
     finally:
         if connection:
             cursor.close()
             connection.close()
-'''
+            
 def process_files(directory):
     for filename in os.listdir(directory):
-        if filename.endswith('.json'):
-            with open(os.path.join(directory, filename), 'r') as file:
-                data = json.load(file)
-                for entry in data:
-                    title = entry.get('title')
-                    content = entry.get('text')
-                    # Call insert_into_database for each article
-                    insert_into_database(id, title, content)
-'''
+        with open(os.path.join(directory, filename), 'r') as file:
+            content = file.read()
+            # Split and process the content to extract article titles and their content
+            # Then call insert_into_database for each article
 
 if __name__ == "__main__":
     process_files('output_directory')
