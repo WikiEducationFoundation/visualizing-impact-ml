48f08fc7001657d27871c6201c36ce4ea772f144
Megan <megan@wikiedu.org>
2023-10-04 18:22:37 +0000

    commenting out the JSON part and adding ID insertion to the query

--- populate_database.py
+++ populate_database.py
@@ -1,7 +1,7 @@
 import psycopg2
 import os
 
-def insert_into_database(article_title, article_content):
+def insert_into_database(id, article_title, article_content):
     try:
         connection = psycopg2.connect(
             database="wikivi"
@@ -9,10 +9,10 @@ def insert_into_database(article_title, article_content):
         cursor = connection.cursor()
 
         insert_query = """
-        INSERT INTO wikipedia_articles (title, content)
+        INSERT INTO wikipedia_articles (id, title, content)
         VALUES (%s, %s);
         """
-        cursor.execute(insert_query, (article_title, article_content))
+        cursor.execute(insert_query, (id, article_title, article_content))
         
         connection.commit()
         
@@ -22,13 +22,20 @@ def insert_into_database(article_title, article_content):
         if connection:
             cursor.close()
             connection.close()
-            
+'''
 def process_files(directory):
     for filename in os.listdir(directory):
-        with open(os.path.join(directory, filename), 'r') as file:
-            content = file.read()
-            # Split and process the content to extract article titles and their content
-            # Then call insert_into_database for each article
+        if filename.endswith('.json'):
+            with open(os.path.join(directory, filename), 'r') as file:
+		data = json.load(file)
+		i = 0
+		for entry in data:
+		    title = entry.get('title')
+		    content = entry.get('text')
+		    id = i
+		    insert_into_database(id, title, content)
+		    i += 1
 
 if __name__ == "__main__":
     process_files('output_directory')
+'''
