1d449d7255e31eb84e0b17c23bbe45dba7963fce
Amine Hassou <japnoo@gmail.com>
2023-11-18 05:45:51 +0100

    Added optional arg parsing to mini embeddings script

--- embedding_mini.py
+++ embedding_mini.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import psycopg2
 import subprocess
@@ -6,18 +7,22 @@ import numpy as np
 
 now = datetime.datetime.now()
 
+parser = argparse.ArgumentParser(description="Process some integers.")
+parser.add_argument('-e', '--embedding_path', default="/home/wikivi/visualizing-impact-ml/llama.cpp/embedding", help="Path to the embeddings")
+parser.add_argument('-m', '--model_path', default="/home/wikivi/visualizing-impact-ml/llama.cpp/models/open_llama_3b_v2/ggml-model-f16.gguf", help="Path to the model file")
+args = parser.parse_args()
+
 conn = psycopg2.connect(dbname="wikivi")
 cursor = conn.cursor()
 cursor.execute("SELECT id, parsed_content from wikipedia_data where parsed_content not like '%redirect%';")
 articles = cursor.fetchall()
-#cursor.execute("ALTER TABLE wikipedia_data ADD COLUMN embeddings TEXT;")
 
 for article_id, content in articles:
     tokens = content.split()[:512]
     tokenized_content = " ".join(tokens)
     with open("temp.txt", "w") as f:
         f.write(tokenized_content)
-    subprocess.run(["/home/wikivi/visualizing-impact-ml/llama.cpp/embedding", "--log-disable", "-p", "temp.txt", "-m", "/home/wikivi/visualizing-impact-ml/llama.cpp/models/open_llama_3b_v2/ggml-model-f16.gguf"], stdout=open('output.vec', 'w'))
+    subprocess.run([args.embedding_path, "--log-disable", "-p", "temp.txt", "-m", args.model_path], stdout=open('output.vec', 'w'))
     with open("output.vec", "r") as f:
         embedding = f.read()
     embedding_list = [float(value) for value in embedding.split()]
