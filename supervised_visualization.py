f02d8942a94c8f12fac0ae42e72f84f3e0c52975
Megan <megan@wikiedu.org>
2023-11-27 15:58:18 +0000

    visualization for labelling data instead of preemptive clustering

---
+++ supervised_visualization.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import psycopg2
+import umap
+import plotly.express as px
+import numpy as np
+
+conn = psycopg2.connect(
+    dbname='wikivi',
+)
+
+query = "SELECT embeddings FROM wikipedia_data where embeddings is not null"
+df = pd.read_sql_query(query, conn)
+
+def parse_embedding(s):
+    s = s.strip('{}')
+    return np.array(s.split(','), dtype=float)
+
+embeddings = np.stack(df['embeddings'].apply(parse_embedding))
+
+reducer = umap.UMAP(random_state=42)
+embedding_2d = reducer.fit_transform(embeddings)
+
+fig = px.scatter(x=embedding_2d[:, 0], y=embedding_2d[:, 1], title="UMAP Visualization of Article Embeddings", labels={'x': 'UMAP Dimension 1', 'y': 'UMAP Dimension 2'})
+fig.update_traces(marker=dict(size=5))
+fig.show()
+
