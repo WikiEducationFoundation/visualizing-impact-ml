2a125f47059a621afc381a87eaf33d9d68f5ff50
Amine Hassou <japnoo@gmail.com>
2023-12-04 23:23:18 +0100

    Removed need for file I/O in llama embeddings generation

--- embedding_mini.py
+++ embedding_mini.py
@@ -21,11 +21,12 @@ for article_id, content in articles:
     print(article_id)
     tokens = content.split()[:512]
     tokenized_content = " ".join(tokens)
-    with open("temp.txt", "w") as f:
-        f.write(tokenized_content)
-    subprocess.run([args.embedding_path, "--log-disable", "-p", "temp.txt", "-m", args.model_path], stdout=open('output.vec', 'w'))
-    with open("output.vec", "r") as f:
-        embedding = f.read()
+
+    # Pass relevant data to the subprocess in-memory without file I/O
+    process = subprocess.Popen([args.embedding_path, "--log-disable", "-p", "-", "-m", args.model_path], 
+                               stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+    embedding, _ = process.communicate(tokenized_content)
+
     embedding_list = [float(value) for value in embedding.split()]
     cursor.execute("UPDATE wikipedia_data SET embeddings = %s WHERE id = %s;", (embedding_list, article_id))
     conn.commit()
