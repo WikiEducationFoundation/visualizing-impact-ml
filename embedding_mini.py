b1881de9f52f6f2a7297a39f6bb72313cfa25bc0
Amine Hassou <japnoo@gmail.com>
2023-12-07 01:20:26 +0100

    Added multi-threading to embeddings script

--- embedding_mini.py
+++ embedding_mini.py
@@ -5,6 +5,31 @@ import subprocess
 import datetime
 import numpy as np
 
+import datetime
+import concurrent.futures
+
+NUM_THREADS = 2
+
+def process_article(article_data, args):
+    article_id, content = article_data
+    try:
+        tokens = content.split()[:512]
+        tokenized_content = " ".join(tokens)
+
+        process = subprocess.Popen([args.embedding_path, "--log-disable", "-p", "-", "-m", args.model_path], 
+                                   stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+        embedding, _ = process.communicate(tokenized_content)
+
+        embedding_list = [float(value) for value in embedding.split()]
+        conn = psycopg2.connect(dbname="wikivi")
+        cursor = conn.cursor()
+        cursor.execute("UPDATE wikipedia_data SET embeddings = %s WHERE id = %s;", (embedding_list, article_id))
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        print(f"Error processing article {article_id}: {e}")
+
 now = datetime.datetime.now()
 
 parser = argparse.ArgumentParser(description="Process some integers.")
@@ -16,25 +41,11 @@ conn = psycopg2.connect(dbname="wikivi")
 cursor = conn.cursor()
 cursor.execute("SELECT id, parsed_content from wikipedia_data where parsed_content not like '%redirect%' and embeddings is null limit 10000;")
 articles = cursor.fetchall()
-
-for article_id, content in articles:
-    print(article_id)
-    tokens = content.split()[:512]
-    tokenized_content = " ".join(tokens)
-
-    # Pass relevant data to the subprocess in-memory without file I/O
-    process = subprocess.Popen([args.embedding_path, "--log-disable", "-p", "-", "-m", args.model_path], 
-                               stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
-    embedding, _ = process.communicate(tokenized_content)
-
-    embedding_list = [float(value) for value in embedding.split()]
-    cursor.execute("UPDATE wikipedia_data SET embeddings = %s WHERE id = %s;", (embedding_list, article_id))
-    conn.commit()
-
 cursor.close()
 conn.close()
-#os.remove("temp.txt")
-#os.remove("output.vec")
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+    executor.map(process_article, articles, [args]*len(articles))
 
 now2 = datetime.datetime.now()
 elapsed = now2 - now
